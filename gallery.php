4f34ac9b90a8312769d206077ec218c157b3b37e
dev <dev@dev.com>
2014-08-25 18:26:09 +0300

    Галерея #2

--- gallery.php
+++ gallery.php
@@ -1,10 +1,7 @@
 <?php include('./includes/body-open.php'); ?>
 <?php include('./includes/header.php'); ?>
-<div class="row">
-	<div class="col-md-3"><?php include('./includes/left-tree.php'); ?></div>
-	<div class="col-md-9">
-		<?php include('./includes/gallery.php'); ?>
-	</div>
-</div>
+
+<?php include('./includes/index.php'); ?>
+	
 <?php include('./includes/footer.php'); ?>
 <?php include('./includes/body-close.php'); ?>
