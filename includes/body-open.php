1a7e84ab90d9070cce92fc4eca57fd5b41b2cbf1
dev <dev@dev.com>
2014-09-26 10:23:03 +0300

    левое меню

--- includes/body-open.php
+++ includes/body-open.php
@@ -1,3 +1,38 @@
+<?php
+	$groups = array(
+		1=>'ПЕРЕГОРОДКИ',
+		2=>'ДВЕРИ',
+		3=>'ИЗДЕЛИЯ ИЗ СТЕКЛА',
+		4=>'ИЗДЕЛИЯ ИЗ СТЕКЛА И СТАЛИ',
+		5=>'ИЗДЕЛИЯ ИЗ ДЕРЕВА',
+		6=>'ХУДОЖЕСТВЕННАЯ ОБРАБОТКА СТЕКЛА',
+	);
+
+	$products = array(
+		1=>array('id'=>1,'label'=>'АЛЮМИНИЕВЫЕ ПЕРЕГОРОДКИ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>1,'label'=>'Алюминиевые')),
+		2=>array('id'=>2,'label'=>'ЦЕЛЬНОСТЕКЛЯННЫЕ ПЕРЕГОРОДКИ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>1,'label'=>'Цельностеклянные (безрамные)')),
+		3=>array('id'=>3,'label'=>'САНТЕХНИЧЕСКИЕ ПЕРЕГОРОДКИ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>1,'label'=>'Сантехнические')),
+		4=>array('id'=>4,'label'=>'ПРОТИВОПОЖАРНЫЕ ПЕРЕГОРОДКИ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>1,'label'=>'Противопожарные')),
+
+		5=>array('id'=>5,'label'=>'ДВЕРИ СТЕКЛЯННЫЕ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Стеклянные распашные')),
+		6=>array('id'=>6,'label'=>'ДВЕРИ ГЛУХИЕ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Глухие распашные')),
+		7=>array('id'=>7,'label'=>'ДВЕРИ ЦЕЛЬНОСТЕКЛЯННЫЕ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Цельностеклянные распашные, маятниковые')),
+		8=>array('id'=>7,'label'=>'ДВЕРИ ЦЕЛЬНОСТЕКЛЯННЫЕ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Цельностеклянные раздвижные')),
+		9=>array('id'=>8,'label'=>'ДВЕРИ МЕТАЛЛИЧЕСКИЕ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Металлические')),
+		10=>array('id'=>9,'label'=>'ВХОДНЫЕ ГРУППЫ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>2,'label'=>'Входные группы (теплые, холодные)')),
+
+		11=>array('id'=>11,'label'=>'ДУШЕВЫЕ КАБИНЫ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>3,'label'=>'Душевые кабины')),
+		12=>array('id'=>12,'label'=>'КУХОННЫЕ ФАРТУКИ (СКИНАЛИ)','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>3,'label'=>'Кухонные фартуки (скинали)')),
+
+		13=>array('id'=>13,'label'=>'СТЕКЛЯННЫЕ ЛЕСТНИЦЫ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>4,'label'=>'Стеклянные лестницы')),
+		14=>array('id'=>14,'label'=>'ЛЕСТНИЧНЫЕ ОГРАЖДЕНИЯ И ПЕРИЛА','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>4,'label'=>'Лестничные ограждения и перила')),
+
+		15=>array('id'=>15,'label'=>'ДЕКОРАТИВНЫЕ НАВЕСНЫЕ ПАНЕЛИ','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>5,'label'=>'Декоративные навесные панели')),
+		16=>array('id'=>16,'label'=>'СТОЙКИ РЕСЕПШН','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>5,'label'=>'Стойки ресепшен')),
+
+		17=>array('id'=>17,'label'=>'ХУДОЖЕСТВЕННАЯ ОБРАБОТКА СТЕКЛА','thumbUrl'=>'','images'=>array(''),'menu'=>array('group'=>6,'label'=>'Художественная обработка стекла')),
+	);
+?>
 <!DOCTYPE html>
 <html>
 	<head>
--- includes/left-tree.php
+++ includes/left-tree.php
@@ -1,25 +1,15 @@
 <ul class="list-group" id="left-tree">
-	<li class="list-group-item">ПЕРЕГОРОДКИ</li>
-	<li class="list-group-item sub">Алюминиевые</li>
-	<li class="list-group-item sub">Цельностеклянные (безрамные)</li>
-	<li class="list-group-item sub">Сантехнические</li>
-	<li class="list-group-item sub">Противопожарные</li>
-	<li class="list-group-item">ДВЕРИ</li>
-	<li class="list-group-item sub">Стеклянные распашные</li>
-	<li class="list-group-item sub">Глухие распашные</li>
-	<li class="list-group-item sub">Цельностеклянные распашные, маятниковые</li>
-	<li class="list-group-item sub">Цельностеклянные раздвижные</li>
-	<li class="list-group-item sub">Металлические</li>
-	<li class="list-group-item sub">Входные группы (теплые, холодные)</li>
-	<li class="list-group-item">ИЗДЕЛИЯ ИЗ СТЕКЛА</li>
-	<li class="list-group-item sub">Душевые кабины</li>
-	<li class="list-group-item sub">Кухонные фартуки (скинали)</li>
-	<li class="list-group-item">ИЗДЕЛИЯ ИЗ СТЕКЛА И СТАЛИ</li>
-	<li class="list-group-item sub">Стеклянные лестницы</li>
-	<li class="list-group-item sub">Лестничные ограждения и перила</li>
-	<li class="list-group-item">ИЗДЕЛИЯ ИЗ ДЕРЕВА</li>
-	<li class="list-group-item sub">Декоративные навесные панели</li>
-	<li class="list-group-item sub">Стойки ресепшен</li>
-	<li class="list-group-item">ХУДОЖЕСТВЕННАЯ ОБРАБОТКА СТЕКЛА</li>
+	<?php foreach ($groups as $gk=>$group) : ?>
+		<li class="list-group-item"><?php echo $group; ?></li>
+		<?php foreach ($products as $product) : ?>
+			<?php if ($product['menu']['group'] == $gk && !empty($product['menu']['label'])) : ?>
+				<li class="list-group-item sub">
+					<a href="/catalog.php?product=<?php echo $product['id']; ?>">
+						<?php echo $product['menu']['label']; ?>
+					</a>
+				</li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	<?php endforeach; ?>
 </ul>
 <p class="left-tree-desc">Представленные фотографии офисных перегородок, межкомнатных дверей, душевых кабин, зеркал и кухонных фартуков (скинали) являются примерами выполняемых компанией "АПИКА Строй" заказов на изготовление и установку подобных конструкций. Внешний вид и технические характеристики каждого заказа полностью зависят от пожеланий заказчика и могут существенно отличаться от представленного на изображениях.</p>
